6e5af43059c01b49dc3b38e2b7f3ee0ddfe6410a
Anya Denisova <bilberian@mail.ru>
2022-12-04 13:08:59 +0300

    homework7

---
+++ task50/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("введите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int m = 3;
+int n = 4;
+
+int[,] matrix = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(0,10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+bool result = false;
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    if (matrix[i,j] == number) result = true;
+}
+
+if(result) { Console.WriteLine($"Найдено ваше число {number}"); }
+else { Console.WriteLine($"{number} => такого числа в массиве нет"); }
---
+++ task52/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int m = 3;
+int n = 4;
+
+int[,] matrix = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.Write("Среднее арифметическое столбца: ");
+double sum = 0;
+double result = 0;
+for (int j = 0; j < n; j++)
+{
+    for (int i = 0; i < m; i++)
+    {
+        sum += matrix[i, j];
+    }
+    result = Math.Round(sum / m, 1);
+    Console.Write($"{result}; ");
+    sum = 0;
+}
